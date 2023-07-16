f9bfad736f0b7eefdbccffd2d80ae10eab0bce9c
khushidadhich <khushidadhich08@gmail.com>
2023-07-16 14:39:14 +0530

    signupform

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form';
+import { Routes,BrowserRouter, Route,Navigate } from 'react-router-dom';
 
 function App() {
+
+
+
   return (
     <div className="App">
-<h1>Productivity Tracker</h1>    
+      <h1>Productivity Tracker</h1>    
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to='/signup'/>} />
+        <Route path="/signup" element={<Form />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
