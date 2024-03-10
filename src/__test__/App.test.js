15f8ee2d75a4ceeae1f6652ae1d1bb59b31511ec
rishii64 <saptarsismmitra@gmail.com>
2024-03-11 01:20:04 +0530

    test cases for login component added

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,7 +1,9 @@
-// OPTIMIZED CODE :
+// OPTIMIZED CODE : //
 import { render, screen, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import SignupForm from '../pages/SignupForm';
+import login from '../pages/login'
+import axios from 'axios'
 
 // mock data --
 const mockUser = {
@@ -9,7 +11,7 @@ const mockUser = {
   email: 'johndoe@gmail.com',
   password: 'securePass123',
 };
-
+/// Testing SIGN UP Component 
 describe('SignUpForm component', () => {
   it('should allow users to sign up with valid information', () => {
     const onSubmitMock = jest.fn();
@@ -36,7 +38,7 @@ describe('SignUpForm component', () => {
   });
 
 
-  //=> Test case for rendering error messages during sign-up
+  // Test case for rendering error messages during sign-up
   test('should render error message for invalid inputs', () => {
     render(<SignupForm />);
     const userNameInput = screen.getAllByLabelText('User name')
@@ -63,7 +65,77 @@ describe('SignUpForm component', () => {
     const passwordError = screen.getByText('Password Required');
     expect(passwordError).toBeInTheDocument()
   });
-
-
-
 });
+
+
+/// Testing LOGIN Component
+describe('LOGIN Component', () => {
+  //=> Testing login component with proper validation
+  test('rendering initial form', () => {
+    render(<login />);
+
+    const heading = screen.getByText(/Hi, Welcome back!/i);
+    expect(heading).toBeInTheDocument();
+
+    const usernameInput = screen.getByRole('textbox', { name: /Email or Username/i });
+    expect(usernameInput).toBeInTheDocument();
+
+    const passwordInput = screen.getByLabelText(/Password/i);
+    expect(passwordInput).toBeInTheDocument();
+
+    const signInButton = screen.getByRole('button', { name: /Sign in/i });
+    expect(signInButton).toBeInTheDocument();
+
+    const rememberMeCheckbox = screen.getByRole('checkbox', { name: /remember/i });
+    expect(rememberMeCheckbox).toBeInTheDocument();
+
+    const forgotPasswordLink = screen.getByText(/Forgot password/i);
+    expect(forgotPasswordLink).toBeInTheDocument();
+
+    const createAccountLink = screen.getByText(/Create an Account/i);
+    expect(createAccountLink).toBeInTheDocument();
+  });
+
+
+  //=> Handli9ng input changes
+  test('handling username input change', () => {
+    render(<login />);
+    // username input change
+    const usernameInput = screen.getByRole('textbox', { name: /Email or Username/i });
+    fireEvent.change(usernameInput, { target: { value: 'testuser' } });
+    expect(usernameInput).toHaveValue('testuser');
+
+    //password input chnage
+    const passwordInput = screen.getByLabelText(/Password/i);
+    fireEvent.change(passwordInput, { target: { value: 'testpassword' } });
+    expect(passwordInput).toHaveValue('testpassword');
+  });
+
+
+  // //=> Handling form submission
+  test('handling form submission with successful login', async () => {
+    const mockResponse = { errMsg: 'Logged in successfully' };
+    jest.spyOn(axios, 'post').mockResolvedValue(mockResponse);
+  
+    render(<login />);
+  
+    const usernameInput = screen.getByRole('textbox', { name: /Email or Username/i });
+    fireEvent.change(usernameInput, { target: { value: 'testuser' } });
+  
+    const passwordInput = screen.getByLabelText(/Password/i);
+    fireEvent.change(passwordInput, { target: { value: 'testpassword' } });
+  
+    const signInButton = screen.getByRole('button', { name: /Sign in/i });
+    await fireEvent.click(signInButton);
+  
+    // verifying API call and expecting response data
+    expect(axios.post).toHaveBeenCalledWith('https://productivitytrackerbe.onrender.com/login', {
+      username: 'testuser',
+      password: 'testpassword'
+    });
+    expect(axios.post).toHaveReturnedValue(mockResponse);
+  });
+  
+
+
+})
